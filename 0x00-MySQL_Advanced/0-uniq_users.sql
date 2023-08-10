6b7fabb7b1346a44fd1a9651d011a513ec5f273d
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2023-08-10 16:14:00 +0300

    We are all unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,1 +1,8 @@
-
+-- Drop table `users` if it exists
+DROP TABLE IF EXISTS `users`;
+-- Create table `users` if it does not exists
+CREATE TABLE IF NOT EXISTS `users`(
+       `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+       `email` VARCHAR(255) UNIQUE NOT NULL,
+       `name` VARCHAR(255)
+);
